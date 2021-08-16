4c1a42c900c50c70a11034ea05377bdc144d397b
akanksha3012 <burnwal.akanksha30@gmail.com>
2021-08-16 23:38:18 +0530

    adding GET methods for book

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
 const { response, request } = require("express");
+
+//database
+const Database = require("./database");
+
 //initialization
 const express = require("express");
 
 const OurAPP = express();
 
 OurAPP.get("/", (request, response) => {
-response.json({message: "Request Served!!!..."});
+response.json({message: "Server is Working!!!..."});
+});
+//Route         -- /book
+//Description   -- to get all books
+//Access        -- public
+//Method        -- GET
+//Params        -- none
+//Body          -- none
+OurAPP.get("/book",(req, res) => {
+    return res.json({books: Database.Book});
+});
+
+//Route         -- /book/:bookID
+//Description   -- to get a book based on ISBN
+//Access        -- public
+//Method        -- GET
+//Params        -- bookID
+//Body          -- none
+OurAPP.get("/book/:bookID",(req, res) => {
+    const getBook = Database.Book.filter((book) =>
+     book.ISBN ===req.params.bookID);
+    return res.json({book: getBook});
+});
+
+//Route         -- /book/c/:category
+//Description   -- to get list of all books based on category
+//Access        -- public
+//Method        -- GET
+//Params        -- category
+//Body          -- none
+OurAPP.get("/book/c/:category",(req, res) => {
+    const getBook = Database.Book.filter((book) =>
+     book.category.includes(req.params.category));
+    return res.json({book: getBook});
+});
+
+//Route         -- /book/a/:author
+//Description   -- to get list of all books based on author
+//Access        -- public
+//Method        -- GET
+//Params        -- category
+//Body          -- none
+// OurAPP.get("/book/a/:Author",(req, res) => {
+//     const getBook = Database.Author.filter((books) =>
+//      Author.name.includes(req.params.Author));
+//     return res.json({books: getBook});
+// });
+
+//Route         -- /book/a/:author
+//Description   -- to get list of all authors
+//Access        -- public
+//Method        -- GET
+//Params        -- category
+//Body          -- none
+OurAPP.get("/author",(req, res) => {
+    return res.json({author: Database.Author});
 });
 
 OurAPP.listen(4000, () => console.log("server is running"));
---
+++ requirement.js
@@ -0,0 +1,79 @@
+/*
+Requirements
+
+Book
+-ISBN             -String
+(international standard book no.)           
+-Title            -String
+-Author           -[Number]
+-Language         -String
+-Publications     -Number
+-NumOfPages       -Number
+-Categories       -Number
+
+Author
+-id             -Number
+-name           -String
+-books          -[String]
+
+Publications
+-id             -Number
+-name           -String
+-books          -[String]
+
+
+----------APIs----------
+
+Book
+-GET
+    -to get all books 
+    -to get specific book
+    -to get list of all books based on category
+    -to get list of books based on author
+
+-POST
+    -to add new book
+
+-PUT
+    -to update book details
+    -to update /add new authors
+
+-DELETE
+    -to delete a book
+    -to delete an author from book
+
+Authors
+-GET
+    -to get all author
+    -to get specific author
+    -to list of author based on a book
+
+-POST
+    -to add new author
+
+-PUT
+    - to update author details
+    -to update /add new authors
+    
+-DELETE
+     -to delete a book
+    -to delete an author from book
+
+Publications
+-GET
+    -to get all publication
+    -to get specific publication
+    -to list of publication based on a book
+
+-POST
+    -to add new publication
+
+-PUT
+    - to update publication details
+    -to update /add new authors
+    
+-DELETE
+     -to delete a book from publication
+    -to delete a publication
+
+*/
